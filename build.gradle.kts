7089ea919454759f89017944f87cd4fcf5457469
Colter23 <13864833022@163.com>
2021-11-20 12:47:32 +0800

    fix: 无法配置仅视频

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "top.colter"
-version = "2.0.1"
+version = "2.0.2"
 
 repositories {
     mavenLocal()
--- src/main/kotlin/top.colter.mirai.plugin.bilibili/PluginMain.kt
+++ src/main/kotlin/top.colter.mirai.plugin.bilibili/PluginMain.kt
@@ -15,7 +15,7 @@ object PluginMain : KotlinPlugin(
     JvmPluginDescription(
         id = "top.colter.bilibili-dynamic-mirai-plugin",
         name = "BilibiliDynamic",
-        version = "2.0.1"
+        version = "2.0.2"
     ) {
         author("Colter")
         info(
