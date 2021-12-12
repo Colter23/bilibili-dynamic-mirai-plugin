c7081bb90dbe8bedc769e29bf5d0d6d7a470f64e
Colter23 <13864833022@163.com>
2021-12-12 18:56:28 +0800

    update: readme

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
 
-    id("net.mamoe.mirai-console") version "2.8.2"
+    id("net.mamoe.mirai-console") version "2.9.0-M1"
 }
 
 group = "top.colter"
