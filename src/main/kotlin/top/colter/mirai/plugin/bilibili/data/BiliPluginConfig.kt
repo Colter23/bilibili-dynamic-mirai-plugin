407920fe70e498842bd9f95aa2e065ea989f888a
Initial-heart <63091661+Initial-heart-1@users.noreply.github.com>
2022-01-27 23:57:18 +0800

    调整配置注释
    万恶微软雅黑

--- src/main/kotlin/top/colter/mirai/plugin/bilibili/data/BiliPluginConfig.kt
+++ src/main/kotlin/top/colter/mirai/plugin/bilibili/data/BiliPluginConfig.kt
@@ -12,22 +12,22 @@ object BiliPluginConfig : ReadOnlyPluginConfig("BiliPluginConfig") {
     @ValueDescription("推送模式\n0: 文字推送\n1: 图片推送")
     val pushMode: Int by value(1)
 
-    @ValueDescription("添加订阅时是否允许bot自动关注未关注的用户")
+    @ValueDescription("添加订阅时是否允许 bot 自动关注未关注的用户")
     val autoFollow: Boolean by value(true)
 
-    @ValueDescription("Bot关注时保存的分组(最长16字符)")
+    @ValueDescription("Bot 关注时保存的分组(最长16字符)")
     val followGroup: String by value("Bot关注")
 
-    @ValueDescription("检测间隔(推荐15-30) 单位秒")
+    @ValueDescription("检测间隔(推荐 15-30) 单位秒")
     val interval: Int by value(15)
 
-    @ValueDescription("图片推送模式用的字体, 详细请看readme")
-    val font: String by value("Microsoft Yahei")
+    @ValueDescription("图片推送模式用的字体, 详细请看 readme")
+    val font: String by value("")
 
     @ValueDescription("直播@全体")
     val liveAtAll: Boolean by value(false)
 
-    @ValueDescription("动态/视频推送文字模板, 参数请看readme")
+    @ValueDescription("动态/视频推送文字模板, 参数请看 readme")
     val pushTemplate: String by value("{name}@{type}\n{link}")
 
     @ValueDescription("直播推送文字模板, 如不配置则与上面的动态推送模板一致")
