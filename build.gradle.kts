3f77f702623a6d682efb1dc442f779e772b5c958
Colter23 <13864833022@163.com>
2022-05-07 15:29:12 +0800

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,6 @@ version = "2.2.2"
 repositories {
     mavenLocal()
     maven("https://maven.aliyun.com/repository/public") // 阿里云国内代理仓库
-    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     maven("https://jitpack.io")
     mavenCentral()
 }
@@ -20,6 +19,5 @@ repositories {
 dependencies{
     implementation("com.vdurmont:emoji-java:5.1.1")
     implementation("com.google.zxing:javase:3.4.1")
-    implementation("org.jetbrains.skiko:skiko-awt-runtime-windows-x64:0.7.18")
     testImplementation(kotlin("test", "1.6.21"))
 }
